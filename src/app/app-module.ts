09c1aa0baf86c5cdd5aa00375ea0c9a84627f685
Fadi brahim <Fadi.ibr@outlook.com>
2025-07-21 15:48:11 -0400

    Initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,40 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Login } from './components/login/login';
+import { SignUp } from './components/sign-up/sign-up';
+import { Home } from './components/home/home';
+import { Products } from './components/products/products';
+import { Cart } from './components/cart/cart';
+import { Categories } from './components/categories/categories';
+import { Brands } from './components/brands/brands';
+import { NotFound } from './components/not-found/not-found';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+
+@NgModule({
+  declarations: [
+    App,
+    Login,
+    SignUp,
+    Home,
+    Products,
+    Cart,
+    Categories,
+    Brands,
+    NotFound,
+    Navbar,
+    Footer
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
